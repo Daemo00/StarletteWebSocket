707a3772b4ceda176eb125181a3d08cefc13fa10
Simone Rubino <daemo00@gmail.com>
2022-11-06 00:14:55 +0100

    Add WS endpoint

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import logging
+
 from starlette.applications import Starlette
-from starlette.endpoints import HTTPEndpoint
-from starlette.routing import Route
+from starlette.endpoints import HTTPEndpoint, WebSocketEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
+from starlette.routing import Route
+from starlette.websockets import WebSocket
+
+logger = logging.getLogger(__name__)
 
 
 class HelloEndpoint(HTTPEndpoint):
@@ -10,8 +15,26 @@ class HelloEndpoint(HTTPEndpoint):
         return Response("<h1>Hello, World!</h1>")
 
 
+class WSEndpoint(WebSocketEndpoint):
+    encoding = "bytes"
+
+    async def on_connect(self, websocket: WebSocket):
+        await websocket.accept()
+
+        logger.info(f"Connected: {websocket}")
+
+    async def on_receive(self, websocket: WebSocket, data: bytes):
+        await websocket.send_bytes(b"OK!")
+
+        logger.info("websockets.received")
+
+    async def on_disconnect(self, websocket: WebSocket, close_code: int):
+        logger.info(f"Disconnected: {websocket}")
+
+
 instance = Starlette(
     routes=(
         Route("/hello", HelloEndpoint, name="hello"),
+        Route("/ws", WSEndpoint, name="ws"),
     ),
 )
